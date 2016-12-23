df21b6e5a307053f08609e9a2585d17a1297e1bb
a-qiang <13526636962@163.com>
2016-12-23 10:20:08 +0800

    updata

---
+++ python_入门/21.while.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# 利用while循环计算100以内奇数的和。
+sun = 0 
+x = 1
+while sun < 100:
+	sun = sun + x
+	print sun
---
+++ python_入门/22.break.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# 例
+sum = 0
+x = 1
+while True:
+    sum = sum + x
+    x = x + 1
+    if x > 100:
+        break
+print sum
+# 题：利用 while True 无限循环配合 break 语句，计算 1 + 2 + 4 + 8 + 16 + ... 的前20项的和。
+sum = 0
+x = 1
+n = 1
+while True:
+    ???
+print sum
+
+
+
